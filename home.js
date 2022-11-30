03a34193b0736ca6da78f7fdffb936d4b260ba18
tjtejas75 <106971429+tjtejas75@users.noreply.github.com>
2022-11-30 15:11:57 +0530

    initial html

---
+++ home.js
@@ -0,0 +1,11 @@
+// console.log('hello');
+// var b = 'smoothie';
+// console.log(b);
+// var age = prompt('What is your name ?');
+// document.getElementById('sometext').innerHTML = 'hey there'+ age;
+function greeting(){
+    var name = prompt('what is your name? ');
+    var result = 'hello ' + name;
+    console.log(result);
+}
+greeting();
